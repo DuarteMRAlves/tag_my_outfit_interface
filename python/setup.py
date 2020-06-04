9594bceba2a34b7ccb29ba6f7f2bd00c5080fa2d
Duarte Alves <Duarte's MBP>
2020-06-04 12:27:42 +0100

    Fixed setup for older setuptools installations

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='outfit-tagging-interface',
     description='grpc code for tag my outfit service',
-    packages=setuptools.find_namespace_packages(include=['outfit_tagging.*']),
+    packages=['outfit_tagging.interface'],
     version='0.0.1',
     python_requires='>=3.6',
     install_requires=['grpcio>=1.16.1']
